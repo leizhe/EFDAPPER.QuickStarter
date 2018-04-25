bb5781f2635d6867d3c45b88d0cceabd94ffac6b
LeiZhe <lz6688@msn.com>
2018-04-25 23:24:46 +0800

    Del Temp

--- ED.CORE.API/ED.WebAPI/Startup.cs
+++ ED.CORE.API/ED.WebAPI/Startup.cs
@@ -1,33 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
 using ED.Application.ServiceImp;
-using ED.Common;
 using ED.Common.Helpers;
 using ED.Common.IoC;
 using ED.Common.Options;
-using ED.Repositories.Core;
 using ED.Repositories.Dapper;
 using ED.Repositories.EntityFramework;
 using log4net;
 using log4net.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.ApplicationParts;
-using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
 using Zxw.Framework.NetCore.Filters;
 
@@ -46,21 +32,8 @@ namespace ED.WebAPI
         }
         
         public IConfiguration Configuration { get; }
-
-        //public IContainer ApplicationContainer { get; private set; }
-
-  
-
-        //public IServiceProvider ConfigureServices(IServiceCollection services)
-        // This method gets called by the runtime. Use this method to add services to the container.
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<EntityFrameworkContext>(options =>
-            //    options.UseSqlServer(Configuration.GetConnectionString("CommandDB")));
-            //Global.CommandDB = Configuration.GetConnectionString("CommandDB");
-            //Global.QueryDB = Configuration.GetConnectionString("QueryDB");
-            //services.a
-            //services.AddMvc().AddControllersAsServices(); 
 
             services.AddMvc(option =>
             {
@@ -79,23 +52,8 @@ namespace ED.WebAPI
                     License = new License { Name = "Use under LICX", Url = "https://example.com/license" }
                 });
             });
-
-            //// Create the container builder.
-            //var builder = new ContainerBuilder();
-
-            ////builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
-
-         
-            //builder.Populate(services);
             
-            //this.ApplicationContainer = builder.Build();
-
-            //// Create the IServiceProvider based on the container.
-            //return new AutofacServiceProvider(this.ApplicationContainer);
             return InitIoC(services);
-
-      
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -145,13 +103,8 @@ namespace ED.WebAPI
             };
             IoCContainer.Register(Configuration);//注册配置
             IoCContainer.Register(dbContextOption);//注册数据库配置信息
-            IoCContainer.Register(typeof(DapperContext));//注册EF上下文
-            IoCContainer.Register(typeof(EntityFrameworkContext));//注册EF上下文
-
-            //var sdsa = Assembly.GetExecutingAssembly();
-            //IoCContainer.Register(Assembly.GetExecutingAssembly());
-            //(typeof(BaseService).Assembly)
-
+            IoCContainer.Register(typeof(DapperContext));
+            IoCContainer.Register(typeof(EntityFrameworkContext));
             IoCContainer.Register(typeof(DapperRepositoryBase<>).Assembly, "QueryRepository");//注册仓储
             IoCContainer.Register(typeof(EntityFrameworkRepositoryBase<>).Assembly, "CommandRepository");//注册仓储
             IoCContainer.Register(typeof(BaseService).Assembly, "Service");
